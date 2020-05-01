be8aea84afad89718ef20314ec801d8f66018263
daiyuanhao <948675173@qq.com>
2020-05-02 01:05:54 +0800

    5.2

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,15 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/tiku-vue/" : "/", //默认为'/',会部署到域名的根路径上; 设为''或'./',这样所有的资源会被链接为相对路径
+  lintOnSave: true,
   devServer: {
     proxy: {
-      '/api': {
+      "/api": {
         target: "http://localhost:3000",
         changeOrigin: true,
         pathRewrite: {
-          '^/api': ''
-        }
-      }
-    }
-  }
-}
+          "^/api": "",
+        },
+      },
+    },
+  },
+};
